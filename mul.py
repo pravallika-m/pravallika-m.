081ea40aa1bf1ac018ead6aa9ffa94b41281b57d
ramnar <raam.soft@gmail.com>
2024-01-26 16:45:16 +0530

    Added multiplication and subtraction of numbers

---
+++ mul.py
@@ -0,0 +1,5 @@
+print('Multipilication of two numbers')
+a=2
+b=100
+product=a*b
+print('Product of two numbers is:',product)
---
+++ sub.py
@@ -0,0 +1,5 @@
+print('subtraction of two  numbers')
+p=20
+q=10
+diff=p-q
+print('Subtraction of two numbers is:',diff)
